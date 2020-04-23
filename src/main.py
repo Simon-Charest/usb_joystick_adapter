4ee2c3a7b317ccb28ba698f0bb4ec2ddb2612b6a
Simon Charest <simoncharest@gmail.com>
2020-04-23 18:24:22 -0400

    Removing debug info

--- src/main.py
+++ src/main.py
@@ -8,14 +8,6 @@ import usb
 
 
 def main():
-    # Debug
-    # usb.manage_usb_joystick_adapter()
-    # files = io_.get_files(constant.FIRMWARE)
-    #
-    # for file in files:
-    #     intel_hex = io_.get_intel_hex(file)
-    #     data = io_.get_data(intel_hex)
-
     ui.run()
 
 
