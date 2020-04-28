e12fb4db22955104d14406c01263a72968e376b9
Simon Charest <simoncharest@gmail.com>
2020-04-27 23:37:43 -0400

    Creating read_device function for libusb library

--- src/libusb_rd.py
+++ src/libusb_rd.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 # coding=utf-8
-
+from distutils.command.config import config
 from tkinter import *
 from tkinter import messagebox
+
+from libusb import context
+
+import constant_rd
 import libusb  # Package: libusb
 
 
@@ -13,4 +17,18 @@ def read_device(devices, device_name, data_label):
         return
 
     """ TODO: This is a test using libusb """
+
+    # Get device (TODO: Remove hardcoding)
+    ctx = []
+    context_ = libusb.init(*ctx)
+
+    if constant_rd.DEBUG:
+        print(f'Devices: {devices}')
+        print(f'Device Name: {device_name}')
+        print(f'Data Label: {data_label}')
+        print(f'Context: {context_}')
+
+    # device = libusb.open_device_with_vid_pid(context_, constant_rd.BOOT_VENDOR_ID, constant_rd.BOOT_PRODUCT_ID)
+    libusb.close()
+
     return
