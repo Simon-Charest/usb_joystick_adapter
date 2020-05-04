599ce133502fec245c839136ddb87253e5e93517
Simon Charest <simoncharest@gmail.com>
2020-05-04 15:46:52 -0400

    Architectural cleanup

--- src/__main__.py
+++
@@ -1,9 +0,0 @@
-from src import app
-
-# Usage:
-#   C:
-#   cd C:\src\usb_joystick_adapter
-#   python -m src
-
-if __name__ == '__main__':
-    app.run()
--- src/app.py
+++
@@ -1,7 +0,0 @@
-from src.cli import cli
-from src.gui import gui
-
-
-def run():
-    # cli.execute()  # Command-Line Interface (CLI)
-    gui.execute()  # Graphical User Interface (GUI)
